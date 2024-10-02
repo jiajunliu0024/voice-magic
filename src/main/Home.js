ed1914c7f2ff67ed94ead6e40535bc431be31f2a
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-10-02 17:48:45 +1000

    Home page of voice cloning

---
+++ src/main/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./main.css";
+
+export default function Home() {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-purple-100 to-blue-100 background-animate">
+      <div className="h-1/2 w-1/2 flex flex-col justify-center items-start">
+        <p className="text-black text-2xl font-bold">
+          Imagine your next AI voice assistant
+        </p>
+        <p className="text-black">
+          Pioneering research in Text to Speech, AI Voice Generator, and more
+        </p>
+      </div>
+      <div className="h-1/2 w-1/2 flex flex-row justify-center">
+        <button className="text-black">Sign in</button>
+        <button className="text-black">join us</button>
+      </div>
+    </div>
+  );
+}
