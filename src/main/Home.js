83912d9474a4a7f501727dccd61e2c46c1382787
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-10-03 18:31:04 +1000

    update the voice bar

--- src/main/Home.js
+++ src/main/Home.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [width, setWidth] = useState(window.innerWidth);
-  const [numberOfBar, setNumberOfBar] = useState(60);
+  const [numberOfBar, setNumberOfBar] = useState(30);
 
   useEffect(() => {
     console.log("width", width);
@@ -16,12 +16,13 @@ export default function Home() {
   }, []);
 
   useEffect(() => {
-    width.width > 1024 ? setNumberOfBar(60) : setNumberOfBar(20);
+    width > 1024 ? setNumberOfBar(30) : setNumberOfBar(15);
+    console.log("numberOfBar", numberOfBar);
   }, [width]);
 
   return (
     <div className="h-screen w-full bg-gradient-to-b from-purple-100 to-blue-100">
-      <div className="pt-40 h-full w-full flex flex-col justify-start items-start">
+      <div className="md:pt-40 h-full w-full flex flex-col justify-start items-start">
         <p className="w-3/4 text-black text-6xl font-semibold text-left px-5">
           Imagine your next AI voice assistant
         </p>
@@ -33,7 +34,7 @@ export default function Home() {
           <button className="ml-4 text-white bg-gray-900 border-10 py-3 px-6 focus:outline-none hover:bg-white hover:text-black hover:scale-110 transition duration-700 ease-in-out rounded-full text-base ">
             Have a try
           </button>
-          <button className="ml-4 text-white bg-gray-900 border-1 py-3 px-6 focus:outline-none hover:bg-gray-200 hover:text-white transition duration-700 ease-in-out rounded-full text-base ">
+          <button className="ml-4 text-white bg-gray-900 border-1 py-3 px-6 focus:outline-none hover:bg-gray-200 hover:text-white transition duration-700 ease-in-out rounded-full text-sm md:text-base ">
             join us in Voice Magic
           </button>
         </div>
