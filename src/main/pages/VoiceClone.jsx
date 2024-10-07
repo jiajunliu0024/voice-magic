ff2f21aa90e40bd31a57047b17b9a1eb59f12008
jiajunliu0024 <jiajunliu0024@gmail.com>
2024-10-08 00:14:10 +1100

    add the button to text to voice page

--- src/main/pages/VoiceClone.jsx
+++ src/main/pages/VoiceClone.jsx
@@ -1,16 +1,29 @@
-import Input from "@mui/icons-material/Input";
 import Box from "@mui/material/Box";
 import Sidebar from "../Sidebar";
 import DrawerHeader from "../components/DrawerHeader";
+import "../main.css";
 
 export default function VoiceClone() {
   return (
     <Box className="flex">
       <Sidebar />
-      <Box>
+      <Box className="w-screen h-full">
         <DrawerHeader />
-        <Input />
         <p>This is Voice Clone</p>
+        <div className="w-full h-auto flex border flex-col justify-center items-center px-5">
+          <textarea
+            className=" w-1/2 h-32 m-10 p-2 border border-gray-300 rounded-md"
+            placeholder="Enter your text here..."
+          ></textarea>
+          <div className="w-1/2 mb-5 flex flex-row space-x-5 justify-items-stretch">
+            <button className="bg-white hover:shadow-rounded border text-black font-bold py-2 px-4 rounded-full transition ease-in duration-300">
+              Generate Voice
+            </button>
+            <button className="bg-white  hover:shadow-rounded border  text-black font-bold py-2 px-4 rounded-full transition ease-in duration-300">
+              select voice
+            </button>
+          </div>
+        </div>
       </Box>
     </Box>
   );
